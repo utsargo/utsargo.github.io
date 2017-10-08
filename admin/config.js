8e23ce101e3e711c9177f820e0983330fc950449
utsargo <royutsargo@gmail.com>
2017-10-08 23:06:34 +0600

    edited css

--- admin/config.js
+++
@@ -1,5 +0,0 @@
-//Place here your personal Access Token, you can go to our Readme if you need help
-	var AccessToken = '869e45618d50e6cc578ae336bd2935d09b15c07e';
-
-//Html tab size
-	var htmlEditorTabSize = '2';
